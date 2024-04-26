8a5dd68180e9a5147188a0ecd0c4b5f407ec1198
QJia <532612154@qq.com>
2024-04-26 09:12:12 +0800

    ci：添加发布脚本

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,6 +5,7 @@ import sitemap from '@astrojs/sitemap';
 
 // https://astro.build/config
 export default defineConfig({
-	site: 'https://example.com',
+	site: 'https://wdzyy.github.io',
 	integrations: [mdx(), sitemap()],
+  base: 'blog',
 });
